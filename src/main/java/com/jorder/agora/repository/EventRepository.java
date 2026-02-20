0f20a16a85605a560dd80491c6bf9382e8f5f2f5
JorderGomes <jordernoveau@gmail.com>
2026-02-19 22:04:18 -0300

    feat: add reposiotries

---
+++ src/main/java/com/jorder/agora/repository/EventRepository.java
@@ -0,0 +1,11 @@
+package com.jorder.agora.repository;
+
+import com.jorder.agora.model.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface EventRepository extends JpaRepository<Event, UUID> {
+}
