3e6bb4eb0a996644636e6cf72666fe1eeb3b377c
JorderGomes <jordernoveau@gmail.com>
2026-02-27 16:28:33 -0300

    fix: update EventResponseDTO properties types

--- src/main/java/com/jorder/agora/dto/EventResponseDTO.java
+++ src/main/java/com/jorder/agora/dto/EventResponseDTO.java
@@ -1,10 +1,13 @@
 package com.jorder.agora.dto;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
 public record EventResponseDTO(
-        String id,
+        UUID id,
         String title,
         String description,
-        String eventDate,
+        LocalDateTime eventDate,
         UserResponseDTO organizer,
         int participantCount
 ) {
